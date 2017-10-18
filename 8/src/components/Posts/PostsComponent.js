ec2453bc076156fdd34ee6736760ea92030025f9
pbartela <2742586+pbartela@users.noreply.github.com>
2017-10-18 14:56:56 +0200

    Added routing

---
+++ 8/src/components/Posts/PostsComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Post } from './../Post/';
+const Posts = props => {
+    const { posts } = props;
+    const postRender = posts.map(function (post, index) {
+        return <Post
+            image={post.image}
+            key={index}
+            username={post.username}
+        >
+            {post.value}
+        </Post>
+    }, this);
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-sm-12'>
+                    <h3>Comments</h3>
+                </div>
+            </div>
+            {postRender}
+        </div>
+    );
+};
+
+Posts.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.shape({
+        username: PropTypes.string,
+        image: PropTypes.string,
+        value: PropTypes.string
+    }))
+};
+
+export default Posts;
