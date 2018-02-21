e6e4fa31691e9ce6e1b90bd068b8b33f916fd13f
Piotr Bartela <pbartela2@future-processing.com>
2018-02-21 13:07:41 +0100

    dependencies updated in all tasks

--- Start/src/index.js
+++ Start/src/index.js
@@ -2,11 +2,11 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 const MainScene = () => {
-        return (
-            <div>
-                Hello world!
-            </div>
-        )
+    return (
+        <div>
+            Hello world!
+        </div>
+    )
 }
 
 ReactDOM.render(<MainScene />, document.getElementById('app'));
