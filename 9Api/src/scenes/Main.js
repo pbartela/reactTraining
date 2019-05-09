1aff8e5708e45261ce00813358e147ee77bdfa8c
tur <turu@turu.pl>
2019-05-09 11:49:54 +0200

    added api call from effect

--- 9Api/src/scenes/Main.js
+++ 9Api/src/scenes/Main.js
@@ -3,28 +3,29 @@ import PropTypes from 'prop-types';
 import Posts from './../components/PostsComponent';
 import TextArea from './../components/TextAreaComponent';
 import TextFilter from './../components/TextFilter';
-import isEqual from 'lodash/isEqual';
 import { addPost, getPosts } from './../api/postsApi'
 
 function MainScene (props) {
     const [posts, setPosts] = useState([]);
     const [filterValue, setFilterValue] = useState('');
 
+    async function fetchData () {
+        const loadedPosts = await getPosts();
+        const postsJson = await loadedPosts.json();
+        setPosts(postsJson);
+    }
+
     useEffect(() => {
-        async function fetchData () {
-            const loadedPosts = await getPosts();
-            const postsJson = await loadedPosts.json();
-            console.log('DEBUG oko: ');
-            console.log('DEBUG posts: ', posts);
-            console.log('DEBUG postsJson: ', postsJson);
-            setPosts(postsJson);
-        }
         fetchData();
-    }, [posts]);
+    }, []);
 
     async function handleSubmit(post) {
-        await addPost(post);
-        setPosts(posts.concat([post]));
+        try {
+            await addPost(post);
+            fetchData();
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     function handleFilterChange(value = '') {
